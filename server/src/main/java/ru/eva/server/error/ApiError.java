b8007211adad327cf543e7cc17f44163938de00c
Danil <dnkuzzin@mail.ru>
2026-09-20 19:04:04 +0700

    fix json error

--- server/src/main/java/ru/eva/server/error/ApiError.java
+++ server/src/main/java/ru/eva/server/error/ApiError.java
@@ -1,7 +1,11 @@
 package ru.eva.server.error;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import java.util.Map;
 
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record ApiError(
         String code,
         String message,
